1b8712361de2449feede5d6566ab34fdb5648ea8
booperlv <siblinggamingplays@gmail.com>
2021-08-29 12:16:00 +0800

    small changes to update nvim script

--- updatenvimnightlybuild.sh
+++ updatenvimnightlybuild.sh
@@ -1,57 +1,60 @@
 #!/bin/bash
 
 # If I find a reason to use 0.6 nightly soon, this might be handy :)
+#You probably shouldn't use this it's pretty unsecure and specific to my
+#installations' circumstances. Also, this isn't optimized much if at all LOL
 
 function install {
-    neovimpath=$(whereis nvim | grep -o '/[^ ]*')
-#You probably shouldn't use this it's pretty unsecure LOL
-    printf 'Downloading Appimage...'
-    wget -q https://github.com/neovim/neovim/releases/download/nightly/nvim.appimage -P ~/.cache
-    printf ' Downloaded Appimage!'
+  neovimpath=$(whereis nvim | grep -o '/[^ ]*')
+  printf 'Downloading Appimage...'
+  wget -q https://github.com/neovim/neovim/releases/download/nightly/nvim.appimage -P ~/.cache
+  printf ' Downloaded Appimage!'
 
-    echo
-    printf 'Removing Old Versions...'
-    pacmancommand=$(sudo pacman -Rns $(pacman -Qsq neovim) --noconfirm &>/dev/null || :)
-    if [ "$pacmancommand" ]; then
-        printf 'Deleting pacman neovim package'
+  echo
+  pacmannvimexists=$(pacman -Qsq neovim)
+  if [ "$pacmannvimexists" ]; then
+    printf 'Found neovim package in Pacman...'
+
+    printf "Would you like to delete the Pacman Package?" ; printf ' [Y/n]' ; read -n 1 -r response
+    if [[ $response =~ ^[Yy]$ ]]; then
+      printf ' Removing Versions from Pacman...'
+      pacmancommand=$(sudo pacman -Rns neovim --noconfirm &>/dev/null || :)
     else
-        printf 'No neovim package found in pacman...'
+      printf ' Not going to remove Package...'
     fi
-    #cleanup some appimages
-    whereis nvim | grep -o '/[^ ]*' | while read -r line; do
-        sudo rm -rf $line
-    done
+  else
+    printf 'No neovim package found in Pacman...'
+  fi
 
-    printf 'Removed Old Versions!'
+  #cleanup some appimages
+  whereis nvim | grep -o '/[^ ]*' | while read -r line; do
+    sudo rm -rf $line
+  done
 
-    echo
-    printf 'Placing Appimage in Correct Directory...'
-    sudo mv ~/.cache/nvim.appimage /usr/local/bin/nvim
-    printf ' Placed Appimage in Correct Directory!'
+  echo
+  printf 'Placing Appimage in Correct Directory...'
+  sudo mv ~/.cache/nvim.appimage /usr/local/bin/nvim
+  printf ' Placed Appimage in Correct Directory!'
 
-    echo
-    printf 'Adding Permissions to New Binaries...'
-    sudo chmod 775 /usr/local/bin/nvim
-    printf ' Permissions Added to New Binaries!'
+  echo
+  printf 'Adding Permissions to New Binaries...'
+  sudo chmod 775 /usr/local/bin/nvim
+  printf ' Permissions Added to New Binaries!'
 
-    echo
-    echo
-    echo "Installed Neovim in $neovimpath"
+  printf "\n\nInstalled Neovim in $neovimpath"
 }
 
+
 function confirminstall {
-    printf "$1" ; printf '[Y/n'] ; read -n 1 -r response
-    if [[ $response =~ ^[Yy]$ ]]; then
-        echo
-        echo
-        echo "Installing..."
-        echo
-        install 
-    else
-        echo
-        echo
-        echo 'Not going to install then :D'
-    fi
+  printf "$1" ; printf ' [Y/n]' ; read -n 1 -r response
+  if [[ $response =~ ^[Yy]$ ]]; then
+    printf "\n\n Installing..."
+    echo
+    echo
+    install 
+  else
+    printf "\n\n Not Installing"
+  fi
 }
 
 currentrelease=$(wget https://api.github.com/repos/neovim/neovim/releases -q -O - | jq -r '.[] | .name' | grep 'NVIM v0.6.0')
@@ -60,14 +63,14 @@ currentversion=$(nvim -v | grep 'NVIM v' || : )
 echo "Current Version is $currentversion"
 
 if [ "$currentversion" ]; then
-    if [[ "${currentrelease}" == "${currentversion}" ]]; then
-        local='Latest Release already installed! Would you still like to Install?'
-        confirminstall "$local"
-    else
-        local='You need to update! Install?' 
-        confirminstall "$local"
-    fi
-else
-    local="We didn't find any nvim binaries in your path. Would you like to install?"
+  if [[ "${currentrelease}" == "${currentversion}" ]]; then
+    local='Latest Release already installed! Would you still like to install?'
     confirminstall "$local"
+  else
+    local='New Update! Would you like to install?' 
+    confirminstall "$local"
+  fi
+else
+  local="We didn't find any nvim binaries in your path. Would you like to install?"
+  confirminstall "$local"
 fi
