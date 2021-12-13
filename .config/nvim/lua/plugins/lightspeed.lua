6b8bd4f86c0bf4b616ebe59a16d7b9cbf550b585
booperlv <siblinggamingplays@gmail.com>
2021-12-13 15:02:10 +0800

    Mon Dec 13 03:02:10 PM +08 2021

--- .config/nvim/lua/plugins/lightspeed.lua
+++ .config/nvim/lua/plugins/lightspeed.lua
@@ -1,8 +1,6 @@
 --Lightspeed
 require'lightspeed'.setup {
-  jump_to_first_match = true,
   limit_ft_matches = 1,
-  full_inclusive_prefix_key = '<leader>',
   --Colemak labels
   labels = {"t", "n", "s", "e", "r", "i", "a", "o", "f", "u",
             "w", "y", "d", "h", "v", "k", "c", "m", "q", ";"},
@@ -10,3 +8,8 @@ require'lightspeed'.setup {
   cycle_group_fwd_key = "<tab>",
   cycle_group_bwd_key = "<s-tab>",
 }
+
+require'mappings'.map({
+  {'n', '<leader>', '<Plug>Lightspeed_x'},
+  {'n', '<S-leader>', '<Plug>Lightspeed_x'},
+})
